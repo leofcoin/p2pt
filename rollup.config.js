6d86da2eb31363e6414399b0c7d7bc2815714734
VandeurenGlenn <vandeurenglenn@gmail.com>
2023-12-16 18:20:08 +0100

    Fix build on windows

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,51 @@
 // import modify from 'rollup-plugin-modify';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
 // import json from '@rollup/plugin-json';
-import rimraf from 'rimraf';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import rimraf from 'rimraf'
+import nodeResolve from '@rollup/plugin-node-resolve'
 // import tsconfig from './tsconfig.json' assert { type: 'json'};
-import {execSync} from 'child_process'
+import { execSync } from 'child_process'
+import { join } from 'path'
 try {
   rimraf.sync('./exports/**/*')
-  execSync('mkdir ./exports/browser')
+  execSync(`mkdir ${join('exports', 'browser')}`)
 } catch (e) {
-  console.log('nothing to clean');
-};
+  console.log('nothing to clean')
+}
 
-
-export default  [{
-  input: ['src/p2pt.ts', 'src/node.ts'],
-  output: [{
-    dir: './exports/commonjs',
-    format: 'cjs'
-  }],
-  external: [
-    'debug',
-    'simple-sha1',
-    'bittorrent-tracker/lib/client/websocket-tracker.js',
-    'simple-websocket',
-    '@koush/wrtc',
-    'wrtc.node'
-  ],
-  plugins: [
-    
-    // commonjs({exclude: ['./simple-peer.js']}),
-    nodeResolve({ preferBuiltins: true }),
-    typescript({ compilerOptions: { declaration: false }}),
-    
-  ]
-}, {
-  input: ['src/p2pt.ts', 'src/node.ts'],
-  output: [{
-    dir: './exports',
-    format: 'es'
-  }],
-  external: [
-    'bittorrent-tracker/lib/client/websocket-tracker',
-    'simple-websocket',
-    '@koush/wrtc',
-    'wrtc.node'
-  ],
-  plugins: [
-    typescript({ compilerOptions: { declarationDir: './exports' }})
-  ]
-}]
+export default [
+  {
+    input: ['src/p2pt.ts', 'src/node.ts'],
+    output: [
+      {
+        dir: './exports/commonjs',
+        format: 'cjs'
+      }
+    ],
+    external: [
+      'debug',
+      'simple-sha1',
+      'bittorrent-tracker/lib/client/websocket-tracker.js',
+      'simple-websocket',
+      '@koush/wrtc',
+      'wrtc.node'
+    ],
+    plugins: [
+      // commonjs({exclude: ['./simple-peer.js']}),
+      nodeResolve({ preferBuiltins: true }),
+      typescript({ compilerOptions: { declaration: false } })
+    ]
+  },
+  {
+    input: ['src/p2pt.ts', 'src/node.ts'],
+    output: [
+      {
+        dir: './exports',
+        format: 'es'
+      }
+    ],
+    external: ['bittorrent-tracker/lib/client/websocket-tracker', 'simple-websocket', '@koush/wrtc', 'wrtc.node'],
+    plugins: [typescript({ compilerOptions: { declarationDir: './exports' } })]
+  }
+]
